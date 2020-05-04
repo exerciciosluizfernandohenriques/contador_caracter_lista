49cf7c67e21c6a81a43112e31b2ebf2706be9e26
Luiz Fernandi <luizfernandosmh@hotmail.com>
2020-05-04 20:45:35 -0300

    Contagem de caracter por lista

---
+++ contador_caracter_lista.py
@@ -0,0 +1,32 @@
+def contador_caracter(texto):
+    """
+    Função que conta os caracteres de uma string
+
+    Ex:
+
+    >>> contar_caracter('luiz')
+    i: 1
+    l: 1
+    u: 1
+    z: 1
+
+    :param texto: string a ser contada
+    """
+
+    caracteres_ordenados = sorted(texto)
+
+    caracter_anterior = caracteres_ordenados[0]
+    contagem = 1
+
+    for caracter in caracteres_ordenados[1:]:
+        if caracter == caracter_anterior:
+            contagem += 1
+        else:
+            print(f'{caracter_anterior}: {contagem}')
+            caracter_anterior = caracter
+            contagem = 1print(f'{caracter_anterior} = {contagem}')
+
+    print(f'{caracter_anterior}: {contagem}')
+
+if __name__ == '__main__':
+    contador_caracter('Banana')
